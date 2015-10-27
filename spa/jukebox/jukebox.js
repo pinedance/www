cbf03ce97e1f793fa9a50cf063a0b4a3f7e07f16
Pinedance <pinedance@gmail.com>
2015-10-28 00:31:53 +0900

    refectoring jukebox

--- spa/jukebox/jukebox.js
+++ spa/jukebox/jukebox.js
@@ -4,16 +4,34 @@ app.filter('secondsToDateTime', [ function() {
     return function(seconds) {
         return new Date(1970, 0, 1).setSeconds(seconds);
     };
-}]) // http://stackoverflow.com/questions/28394572/angularjs-seconds-to-time-filter
+}]); // http://stackoverflow.com/questions/28394572/angularjs-seconds-to-time-filter
 
-app.controller('mainCtrl', ['$scope', "$firebase", "$interval", "$window", "$cookieStore", "loginSession",
-function($scope, $firebase, $interval, $window, $cookieStore, loginSession){
+app.constant('Cons', {
+    firebase: {
+        root: "https://brilliant-torch-7744.firebaseio.com/"
+    }
+})
 
-// login logout
+app.value('initData', {
+    auth: {
+        message: { 
+            title: 'login',
+            message: 'input password',
+            input: true,
+            value: 'welcome!',
+            label: 'answer?'
+        }
+    }
+});
+    
+app.controller('mainCtrl', ['$scope', "$firebase", "$interval", "$window", "$cookieStore", "loginSession", "initData", "Cons",
+function($scope, $firebase, $interval, $window, $cookieStore, loginSession, initData, Cons){
+
+    // login logout
 
     $scope.login = function(){ 
-        defaultMsg.value = "welcome!"
-        loginSession(defaultMsg, promptInputSucceed, promptInputFailed)
+        initData.auth.message.value = "welcome!"
+        loginSession(initData.auth.message, promptInputSucceed, promptInputFailed)
     }
     
     $scope.logout = function(){ 
@@ -21,14 +39,6 @@ function($scope, $firebase, $interval, $window, $cookieStore, loginSession){
         $scope.jukebox.ctrl.pause()
         $scope.loginState = $cookieStore.get('login') ; 
         $window.location.reload();  // http://stackoverflow.com/questions/21885518/angularjs-reload-page
-    }
-    
-    var defaultMsg = { 
-        title: 'login',
-        message: 'input password',
-        input: true,
-        value: 'welcome!',
-        label: 'answer?'
     }
     
     function failMessage(){
@@ -51,20 +61,42 @@ function($scope, $firebase, $interval, $window, $cookieStore, loginSession){
     
 // init
 
-    $scope.loginState = $cookieStore.get('login')
-    
-    var isMobile = (function isMobileOrTablet() {
-          var check = false;
-          (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
-        return check;
-    }()); // http://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+    var rindex = [];
     
     var ref, mainObj
     
     linkFirebase()
     
+    $scope.loginState = $cookieStore.get('login')
+    
+    $scope.newSong = {
+       'title': "",
+       'artist': "", 
+       'url': "",
+       'type': "youtube",
+    };
+
+    $scope.jukebox = {
+        state: {}
+    }
+
+    $scope.random = false;
+
+    $scope.randomize = function(){
+        $scope.random = !$scope.random;
+        if($scope.random){
+            rindex = $scope.songs.keys().suffle();
+        }
+    };
+    
+    // var isMobile = (function isMobileOrTablet() {
+    //       var check = false;
+    //       (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+    //     return check;
+    // }()); // http://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+    
     function linkFirebase(){
-        ref = new Firebase("https://brilliant-torch-7744.firebaseio.com/"); 
+        ref = new Firebase( Cons.firebase.root ); 
         mainObj = $firebase(ref).$asObject();
         mainObj.$bindTo($scope, "data").then(function() {
             setPlayLists();
@@ -87,36 +119,13 @@ function($scope, $firebase, $interval, $window, $cookieStore, loginSession){
         return true;
     };
     
-    var rindex = [];
-    
-    (function init(){
-        $scope.newSong = {
-           'title': "",
-           'artist': "", 
-           'url': "",
-           'type': "youtube",
-        };
-
-        $scope.jukebox = {
-            state: {}
-        }
-
-        $scope.random = false;
-
-        $scope.randomize = function(){
-            $scope.random = !$scope.random;
-            if($scope.random){
-                rindex = $scope.songs.keys().suffle();
-            }
-        };
-    }());
-
    ///////////////////////////// jukebox ////////////////////////////////
     
     $scope.$on('onPlayerReady', function(event, data){
+        console.log("on player ready")
         $scope.jukebox.state = {
             isPlay: function(){
-                if(this.now === 1){return true};
+                if(this.now == 1){return true};
                 return false;
             },
             isMuted: function(){
@@ -127,13 +136,10 @@ function($scope, $firebase, $interval, $window, $cookieStore, loginSession){
                 return (data.target.getCurrentTime() / this.runTime * 100) + '%';
             }
         }
-        $interval( function(){ data.target.getCurrentTime() }, 500, false);
-        console.log("on player ready")
-        $scope.$apply(function(){
-            $scope.jukebox.state.now = data.target.getPlayerState();
-        })
-        
-        // if(isMobile){return}  // 모바일인 경우 자동 재생 되지 않으므로 정지시킴
+        $interval( function(){ 
+            data.target.getCurrentTime() 
+        }, 500, false);
+
         if($scope.loginState){
             $scope.jukebox.ctrl.play();
         }
@@ -143,11 +149,10 @@ function($scope, $firebase, $interval, $window, $cookieStore, loginSession){
     $scope.$on('onPlayerStateChange', function (event, data) {
         
         if(data.playerState == 0){ $scope.jukebox.ctrl.next(); }
-        $scope.$apply(function(){
+        // $scope.$apply(function(){
             $scope.jukebox.state.now = data.playerState;
-        })
+        // })
         console.log("player state: " + $scope.jukebox.state.now)
-        
     });
     
     $scope.$on('onPlayerError', function (event, data) {
